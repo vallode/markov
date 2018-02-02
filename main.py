7f516bf60a367285f91903ec14cfd7875b1193d8
Mariusz Zarzycki <vallode@hotmail.co.uk>
2018-02-02 03:57:18 +0000

    Refactored dicts into a class

--- main.py
+++ main.py
@@ -1,137 +1,136 @@
 import os
 import re
-import random
+import threading
 import numpy as np
 
 
 class Markov:
 
     def __init__(self):
-        with open(os.path.dirname(os.path.realpath(__file__)) + '/text.txt') as file:
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/Te_Deum.txt') as file:
             self.text = file.read()
 
         self.length = {}
-        self.letters = {}
-        self.stems = {}
-        self.bigrams = {}
-        self.trigrams = {}
-        self.count = 0
+        self.nodes = {}
 
     def map_characters(self):
-        self.text = re.sub(r'[.,;:*%$#@!?><\[\]\n]', ' ', self.text).lower()
+        self.text = re.sub(r'\W', ' ', self.text).lower()
 
-        for character in self.text:
-            if character in self.letters or character == ' ':
-                continue
-            else:
-                self.letters[character] = {}
-                self.bigrams[character] = {}
+        Node.count = len(self.text.replace(' ', ''))
+        Node.stems = len(self.text.split())
 
-        for letter in re.sub(r'\B.| ', '', self.text).lower():
-            if letter in self.stems:
-                continue
+        for character in self.text.replace(' ', ''):
+            if character in self.nodes:
+                self.nodes[character].add()
             else:
-                self.stems[letter] = 0
+                self.nodes[character] = Node(character, 0)
 
-        for word in re.findall(r'\w+', self.text):
-            if len(word) not in self.length:
-                self.length[len(word)] = 1
+        for word in self.text.split():
+            word_length = len(word)
+            if word_length in self.length:
+                self.length[word_length] += 1
             else:
-                self.length[len(word)] += 1
+                self.length[word_length] = 1
+
+            stem = word[0]
+            self.nodes[stem].stem()
 
     def states(self):
-        for letter in self.letters:
-            pairs = re.findall(r'({}.?)'.format(letter), self.text)
+        for node in self.nodes.values():
+            proceeding = re.findall(r'(?<={}).'.format(node.data()['name']), self.text)
 
-            for bigram in pairs:
-                if bigram[-1] != ' ':
-                    if bigram not in self.bigrams[letter]:
-                        self.bigrams[letter][bigram] = 1
-                    else:
-                        self.bigrams[letter][bigram] += 1
+            for character in proceeding:
+                node.link(character)
 
-                    if bigram not in self.trigrams:
-                        self.trigrams[bigram] = {}
+            bigrams = re.findall(r'{}(?! ).?'.format(node.data()['name']), self.text)
 
-                if bigram[-1] not in self.letters[letter]:
-                    self.letters[letter][bigram[-1]] = 0
-                else:
-                    self.letters[letter][bigram[-1]] += 1
-
-        for letter in self.stems:
-            expression = re.compile(r'{}'.format(letter))
-            stems = re.sub(r'\B.| ', '', self.text).lower()
-
-            pairs = expression.findall(stems)
-
-            for character in pairs:
-                if character not in self.stems:
-                    self.stems[letter] = 1
-                else:
-                    self.stems[letter] += 1
-
-        for bigram in self.trigrams:
-            pairs = re.findall(r'({}.?)'.format(bigram), self.text)
-
-            for trigram in pairs:
-                if trigram[-1] != ' ':
-                    if trigram not in self.trigrams[bigram]:
-                        self.trigrams[bigram][trigram] = 1
-                    else:
-                        self.trigrams[bigram][trigram] += 1
+            for bigram in bigrams:
+                node.bigram(bigram)
 
     def generate(self, words):
         for x in range(words):
+            word = ''
             lengths = list(self.length.keys())
 
             length_probabilities = list(map(lambda x: x / sum(self.length.values()), self.length.values()))
-            length = np.random.choice(lengths, p=length_probabilities)
+            word_length = np.random.choice(a=lengths, p=length_probabilities)
 
-            stems = list(self.stems.keys())
+            stem_probabilities = []
+            stems = []
+            for node in markov.nodes:
+                stem_probabilities.append(self.nodes[node].data()['stem_weight'])
+                stems.append(self.nodes[node].data()['name'])
 
-            stem_probabilities = list(map(lambda x: x / sum(self.stems.values()), self.stems.values()))
-            stem = np.random.choice(stems, p=stem_probabilities)
+            stem = np.random.choice(a=stems, p=stem_probabilities)
 
-            # print('Word stem: ', stem)
+            node = markov.nodes[stem].data()
 
-            bigram_probabilies = list(map(lambda x: x / sum(self.bigrams[stem].values()), self.bigrams[stem].values()))
-            bigram = np.random.choice(list(self.bigrams[stem].keys()), p=bigram_probabilies)
+            bigrams = list(node['bigrams'].keys())
+            bigram_probabilies = list(map(lambda x: x / sum(node['bigrams'].values()), node['bigrams'].values()))
 
-            # print('Stem bigram: ', bigram)
+            bigram = np.random.choice(a=bigrams, p=bigram_probabilies)
 
-            state = bigram
-            word = ''
+            state = bigram[1]
+            word += bigram
 
             while True:
-                # print('Current state: ', state)
-                word += state
-                # print('Word: ', word)
+                node = markov.nodes[state].data()
 
-                if len(state) > 1 and state[-1] != ' ':
-                    state = state[-1]
+                links = list(node['links'].keys())
+                link_weights = list(map(lambda x: x / sum(node['links'].values()), node['links'].values()))
 
-                weighed_probabilities = list(map(lambda x: x / sum(self.letters[state].values()), self.letters[state].values()))
-                characters = list(self.letters[state].keys())
-                next_character = np.random.choice(characters, p=weighed_probabilities)
+                next_state = np.random.choice(a=links, p=link_weights)
 
-                while len(word) < 4 and next_character == ' ':
-                    next_character = np.random.choice(characters, p=weighed_probabilities)
+                while len(next_state) < word_length and next_state == ' ':
+                    next_state = np.random.choice(a=links, p=link_weights)
 
-                if state == next_character:
-                    bigram = state + next_character
-
-                    trigram_probabilies = list(map(lambda x: x / sum(self.trigrams[bigram].values()), self.trigrams[bigram].values()))
-                    trigram = np.random.choice(list(self.trigrams[bigram].keys()), p=trigram_probabilies)
-                    # print('Found equals: ' + state, next_character)
-                    # print('Trigram: ' + trigram)
-                    state = trigram[-1]
+                if next_state == ' ' or len(word) >= word_length:
+                    break
                 else:
-                    if next_character == ' ' or len(word) >= length:
-                        break
-                    else:
-                        state = next_character
+                    state = next_state
+                    word += state
 
             print(word)
+
+
+class Node:
+    count = 0
+    stems = 0
+
+    def __init__(self, character, freq):
+        self.name = character
+        self.frequency = freq
+        self.stem_frequency = 0
+
+        self.weight = 0
+        self.stem_weight = 0
+
+        self.bigrams = {}
+        self.trigrams = {}
+        self.links = {}
+
+    def link(self, character):
+        if character in self.links:
+            self.links[character] += 1
+        else:
+            self.links[character] = 1
+
+    def bigram(self, bigram):
+        if bigram in self.bigrams:
+            self.bigrams[bigram] += 1
+        else:
+            self.bigrams[bigram] = 1
+
+    def stem(self):
+        self.stem_frequency += 1
+        self.stem_weight = self.stem_frequency / Node.stems
+
+    def add(self):
+        self.frequency += + 1
+        self.weight = self.frequency / Node.count
+
+    def data(self):
+        return self.__dict__
 
 
 if __name__ == "__main__":
@@ -139,5 +138,4 @@ if __name__ == "__main__":
 
     markov.map_characters()
     markov.states()
-    print(markov.length)
     markov.generate(100)
